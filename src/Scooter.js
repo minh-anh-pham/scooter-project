70f116039a57bfd7eb80be95dbb6ac5c1d47d4a3
Minh Anh Pham <minhanhpham@fb.com>
2022-10-24 03:45:13 +0100

    Add to Scooter.js properties station, user, serial, charge, isBroken, docked as well as rent() method

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,13 @@
 class Scooter{
   // scooter code here
+  constructor (station, user) {
+    this.station = station;
+    this.user = user;
+    this.serial = Math.floor(Math.random() * 1001);
+    this.charge = Math.floor(Math.random() * 101);
+    this.isBroken = false;
+    this.docked = true;
+  }
 }
 
 
