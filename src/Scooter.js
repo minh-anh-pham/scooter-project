c11258a0da50dff7f9b1fece627ab25762133c45
Minh Anh Pham <minhanhpham@fb.com>
2022-10-24 04:01:58 +0100

    Add to Scooter.js methods recharge() and requestRepair() with setInterval timers

--- src/Scooter.js
+++ src/Scooter.js
@@ -8,7 +8,65 @@ class Scooter{
     this.isBroken = false;
     this.docked = true;
   }
-}
 
+  rent() {
+    if (this.isBroken === false && this.charge > 20) {
+      this.docked = false;
+      console.log("Enjoy the ride!");
+    }
+    else if (this.charge <= 20) {
+      throw new Error ("Scooter low on battery, please charge.");
+    }
+    else {
+      throw new Error ("Scooter is broken, please send a repair request.");
+    }
+  }
+
+  dock (station) {
+    if (station === undefined) {
+      throw new Error ("Docking station required!");
+    }
+    else {
+      this.station = station;
+      this.docked = true;
+      this.user = "";
+    }
+  }
+
+  recharge() {
+    async function charge() {
+      console.log("Starting charge");
+
+      // wait 2 seconds
+      await new Promise(resolve => setTimeout(resolve, 2000));
+
+      this.charge = 100;
+
+      console.log("Charging complete");
+    }
+
+    charge();
+
+    this.charge = 100;
+  }
+
+  requestRepair() {
+    async function repair() {
+      console.log("Requesting repair");
+
+      // wait 2 seconds
+      await new Promise(resolve => setTimeout(resolve, 2000));
+
+      this.charge = 100;
+
+      console.log("Repair complete");
+    }
+
+    repair();
+
+    this.isBroken = false;
+  }
+
+}
 
 module.exports = Scooter
