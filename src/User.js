be57b7bb9c5d82f7acfc79c5c7fa27da1d964ddd
Minh Anh Pham <minhanhpham@fb.com>
2022-10-24 01:55:43 +0100

    fill in User.js with username, password, and age as well as error if age is < 18

--- src/User.js
+++ src/User.js
@@ -1,5 +1,13 @@
 class User {
   // User code here
+  constructor (username, password, age) {
+    if (age < 18) {
+      throw new Error ("You must be 18 years old or older to ride a scooter");
+    }
+    this.username = username;
+    this.password = password;
+    this.age = age;
+  }
 }
 
-module.exports = User
+module.exports = User;
